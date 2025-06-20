e56e4867b51ff63caa9a782f769e4d14b6a73f27
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-06-20 09:39:25 -0500

    New dataset new model

--- embeddings.py
+++ embeddings.py
@@ -8,22 +8,22 @@ import plotly.express as px
 df = pd.read_csv("topmovies.csv")
 
 #only grab relevant columns
-df_subset = df[["title", "genres", "keywords", "overview"]]
+df_subset = df[["title", "genres", "keywords", "overview", "release_date"]]
 #fill in missing cols
 df_subset = df_subset.fillna("")
 
 df_subset["metadata"] = df_subset["title"] + " " + df_subset["overview"] + " " + df_subset["genres"] + " " + df_subset["keywords"]
 #load embedding model
-print(df['metadata'].head())
+print(df_subset["metadata"].head())
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 
 print("Making embeddings")
-embeddings = model.encode(df['metadata'].tolist(), show_progress_bar = True)
+embeddings = model.encode(df_subset['metadata'].tolist(), show_progress_bar = True)
 
 #save embeddings then dataframe 
 np.save("movie_embeddings.npy", embeddings)
-joblib.dump(df, "movie_metadata.pkl")
+joblib.dump(df_subset, "movie_metadata.pkl")
 
 print("Embeddings saved")
