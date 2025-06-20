c3f93fc74122def531a82dc940b4cdb4b19a1958
Henry Nguyen <165993917+henrynguyen50@users.noreply.github.com>
2025-06-20 10:35:57 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import requests
-query = "I want something like interstellar"
-res = requests.post("http://127.0.0.1:5000/recommend", json={
-    "query": query
-})
-print(f"{query}")
-print(res.json())
