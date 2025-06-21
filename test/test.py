374b7b9548ac53d864252adc80ab432ab827eaf2
Henry Nguyen <165993917+henrynguyen50@users.noreply.github.com>
2025-06-21 17:31:09 -0500

    Delete test directory

--- test/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-query = "I want something like the nice guys comedy action"
-res = requests.post("http://127.0.0.1:5000/recommend", json={
-    "query": query
-})
-print(f"{query}")
-print(res.json())
